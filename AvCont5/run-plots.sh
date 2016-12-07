dfeb73c4a9fe64ac1802aa9c6790f47d11062c23
hermetico <yojmarinv@gmail.com>
2016-12-07 15:50:02 +0100

    plots and stuff

--- AvCont5/run-plots.sh
+++ AvCont5/run-plots.sh
@@ -1,11 +1,44 @@
 #!/bin/bash
-DATAFILE=${1:-out.dat}
+
+DATAFILE="out-d.dat"
 
 gnuplot << GPLOT
-set key autotitle columnhead
-set terminal pdf
-set output 'plot.pdf'
-set xlabel "Number"
-set ylabel "Compressed length"
-plot "$DATAFILE" u 1:2 with lines title "length"
+	set grid xtics mxtics ytics
+	set mxtics 2
+	set grid	
+	set key autotitle columnhead
+	set terminal postscript enhanced color eps
+	set output 'plot-d.eps'
+	set xlabel "Number"
+	set ylabel "Compressed length"
+	set xrange [-1250:1250]
+	plot "$DATAFILE" u 1:2 with lines title "m = 32", \
+	"$DATAFILE" u 1:3 with lines title "m = 64", \
+	"$DATAFILE" u 1:4 with lines title "m = 128", \
+	"$DATAFILE" u (\$1):(11) with lines title "Binary" lt -1, \
+#	"$DATAFILE" u 1:5 with lines title "m = 256", \
+#	"$DATAFILE" u 1:6 with lines title "m = 512", \
+
+GPLOT
+DATAFILE="out-e.dat"
+
+gnuplot << GPLOT
+	set grid xtics mxtics ytics mytics
+	set mxtics 2
+	set mytics 2
+	set grid	
+	set key autotitle columnhead
+	set terminal postscript enhanced color eps
+	set output 'plot-e.eps'
+	set xlabel "Number"
+	set ylabel "Compressed length"
+	set xrange [-50:50]
+	set yrange [0:15]
+	plot "$DATAFILE" u 1:2 with lines title "m = 4", \
+	"$DATAFILE" u 1:3 with lines title "m = 8", \
+	"$DATAFILE" u 1:4 with lines title "m = 16", \
+	"$DATAFILE" u 1:5 with lines title "m = 32", \
+	"$DATAFILE" u 1:6 with lines title "m = 64", \
+	"$DATAFILE" u (\$1):(11) with lines title "Binary" lt -1, \
+
 GPLOT
