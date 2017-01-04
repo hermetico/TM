6f1c09173e6e5d0c70ee92e291b39a6ccc6042f3
hermetico <yojmarinv@gmail.com>
2017-01-04 21:00:57 +0100

    base encoder, encoded images and DVector

--- Project/src/main/java/project/encoder/Encoder.java
+++ Project/src/main/java/project/encoder/Encoder.java
@@ -1,22 +1,50 @@
-package project.processor;
+package project.encoder;
 
 import java.awt.image.BufferedImage;
-import java.util.List;
-import java.util.zip.ZipEntry;
+import java.awt.image.WritableRaster;
 import project.counters.Counters;
 import project.input.Unzip;
 import project.misc.Tracer;
 import project.player.Player;
+import project.processor.Buffer;
 import project.settings.Configuration;
+import project.settings.Setup;
 
 public class Encoder {
     protected Configuration cf;
     protected Tracer tr;
     protected Unzip zp;
 
-    protected Buffer<BufferedImage> buffer;
+    protected Buffer<EncodedImage> buffer;
     protected Counters counters;
-    protected Player player;
+    protected Player player = null;
+    protected Setup setup;
 
+    public Encoder(Setup setup) {
+        cf = Configuration.getInstance();
+        tr =  Tracer.getInstance();
+        counters = new Counters();
+        this.setup = setup;
+    }
+
+    public void setPlayer(Player player){
+        this.player = player;
+        this.player.setBuffer(buffer);
+    }
     
+    public void setupEncoder(int bufferSize){
+        this.buffer = new Buffer<EncodedImage>(bufferSize);
+    
+    }
+    public void startUpEncoderPlayer(){
+        if(player != null) this.player.playLoop();
+    }
+    public void encode(BufferedImage image){
+        
+        // let's do a copy of the image
+        BufferedImage encoded = new BufferedImage(image.getColorModel(), (WritableRaster) image.getData(), image.isAlphaPremultiplied(), null);
+        // assume all images are I
+        EncodedImage ei = new ImageI(encoded);
+        buffer.add(ei);
+    }
 }
